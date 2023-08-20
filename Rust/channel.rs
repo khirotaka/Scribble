694b80c8d82fbb321c7fce7621ce81919e68cfba
Hirotaka Kawashima <k.hirotaka@me.com>
2023-08-20 13:05:27 +0000

    joinを追加

--- Rust/channel.rs
+++ Rust/channel.rs
@@ -4,11 +4,13 @@ use std::thread;
 fn main() {
     let (sender, revicer) = mpsc::channel();
 
-    thread::spawn(move ||{
+    let th = thread::spawn(move ||{
         let val = String::from("World!");
         sender.send(val).unwrap();
     });
 
     let reviced = revicer.recv().unwrap();
+
+    th.join().unwrap();
     println!("Hello, {}", reviced);
 }
